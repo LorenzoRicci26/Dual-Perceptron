48397a3d07d0bc0428927ee927f8b03189aca0b2
LorenzoRicci26 <lorenzo.ricci15@stud.unifi.it>
2023-04-03 20:29:31 +0200

    addition of 2 new dataset

--- main.py
+++ main.py
@@ -10,25 +10,82 @@ from KernelFunction import KernelFunctions
 
 
 def main():
-
-    kf = KernelFunctions()
-    df = pd.read_csv('cmp/iris_data.csv', names = ["Sepal Lenght", "Sepal Width", "Petal Lenght", "Petal Width", "Class"])
-
-    le = preprocessing.LabelEncoder()
-    for i in range(df.shape[1] - 1):
-        newvals = le.fit_transform(df.iloc[:, i])
-        df[df.columns[i]] = newvals
     
-    X = df.iloc[: , :- 1].values #Data
-    y = df.iloc[:, -1].values #Target
-    y = np.where ( y == 'Iris-setosa', -1, 1)
+    #Inizializzo 
+    kf = KernelFunctions()
+    le = preprocessing.LabelEncoder()
+    
+    #Input per scegliere quale dataset si desidera utilizzare
+    print('Quale dataSet desideri utilizzare? | 1: Iris,  2: Adult,  3: Bank')
+    df_type = input('Scegli 1,2 o 3 : ')
+
+    if df_type == '1':
+
+        df = pd.read_csv('cmp/iris_data.csv', names = ["Sepal Lenght", "Sepal Width", "Petal Lenght", "Petal Width", "Class"])
+
+        X = df.iloc[: , :- 1].values #Data
+        print('DataSet: ' + str(X) + str(X.shape))
+        y = df.iloc[:, -1].values #Target
+        y = np.where ( y == 'Iris-setosa', -1, 1)
+        print('Target: ' + str(y) + str(y.shape))
+
+    elif df_type == '2':
+
+        df = pd.read_csv('cmp\_adult_data.csv')
+
+        #Trasformo le colonne in dati numerici 
+        for i in range(df.shape[1] - 1):
+            newvals = le.fit_transform(df.iloc[:, i])
+            df[df.columns[i]] = newvals
+
+        X = df.iloc[: , :-1].values
+        print('DataSet: ' + str(X) + str(X.shape))
+        y = df.iloc[: , -1].values
+        y = np.where( y == '<=50K', -1, 1)
+        print('Target: ' + str(y) + str(y.shape))
+
+    elif df_type == '3':
+
+        df = pd.read_csv('cmp\_bank-additional-full.csv', sep=';')
+
+        #Trasformo le colonne in dati numerici 
+        for i in range(df.shape[1] - 1):
+            newvals = le.fit_transform(df.iloc[:, i])
+            df[df.columns[i]] = newvals
+
+        X = df.iloc[: , :4].values
+        print('DataSet: ' + str(X) + str(X.shape))
+        y = df.iloc[: , -1].values
+        y = np.where( y == 'no', -1, 1)
+        print('Target: ' + str(y) + str(y.shape))
+
+    else:
+        print('Hai inserito un numero sbagliato !!!')
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
 
-    #Vado a mappare il mio set di dati attraverso un metodo kernel 
-    X_train_mapped = kf.rbf_kernel(X_train)
-    X_test_mapped = kf.rbf_kernel(X_test)
+    print('Data_train: ' + str(X_train) + str(X_train.shape))
+    print('Data_test: ' + str(X_test) + str(X_test.shape))
+    print('Target_train: ' + str(y_train) + str(y_train.shape))
+    print('Target_test: ' + str(y_test) + str(y_test.shape))
 
+    #Input per scegliere quale tipo di mappatura kernel di vuole utilizzare
+    print('Quale tipo di mappatura kernel desideri utilizzare? | 1: linear,  2: polynomial,  3: rbf')
+    kernel_type = input('Scegli 1, 2 o 3 : ')
+    
+    #Vado a mappare il mio set di dati attraverso il metodo kernel selezionato dall'utente
+    if kernel_type == '1':
+        X_train_mapped = kf.linear_kernel(X_train)
+        X_test_mapped = kf.linear_kernel(X_test)
+    elif kernel_type == '2':
+        X_train_mapped = kf.polynomial_kernel(X_train)
+        X_test_mapped = kf.polynomial_kernel(X_test)
+    elif kernel_type == '3':
+        X_train_mapped = kf.rbf_kernel(X_train)
+        X_test_mapped = kf.rbf_kernel(X_test)
+    else:
+        print('Hai inserito un numero sbagliato !!!')
+        
     perceptron = MyDualPerceptron(X_train_mapped, X_test_mapped)
     
     perceptron.train(X_train, y_train)
@@ -40,12 +97,6 @@ def main():
 
     accuracy = accuracy_score(y_test, y_pred)
     print('Accuracy:', accuracy)
-    
-    """
-    y_test = y_test.astype(int)
-    fpr, tpr, thresholds = metrics.roc_curve(y_test, y_pred)
-    print(metrics.auc(fpr, tpr))
-    print("ACCURACY:", metrics.accuracy_score(y_test, y_pred))
-    """
+
 if __name__ == "__main__":
     main()
