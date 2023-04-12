cfe1bb0b2f85e08ca4d5b164a07f768e53ee18ba
LorenzoRicci26 <lorenzo.ricci15@stud.unifi.it>
2023-04-12 23:05:14 +0200

    modificata relazione

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn import preprocessing, metrics
+from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import timeit
