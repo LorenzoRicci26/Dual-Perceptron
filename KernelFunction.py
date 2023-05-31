319a7ada8922728f42c9bb5d4b2c6124318359cd
LorenzoRicci26 <lorenzo.ricci15@stud.unifi.it>
2023-05-31 17:02:53 +0200

    completed

--- KernelFunction.py
+++ KernelFunction.py
@@ -11,7 +11,7 @@ class KernelFunctions:
     def polynomial_kernel(self, x1, x2, degree=2):
         return (np.dot(x1, x2) + 1)**degree
 
-    def rbf_kernel(self, x1, x2, gamma=0.15):
+    def rbf_kernel(self, x1, x2, gamma=0.1):
         distance = np.linalg.norm(x1 - x2) ** 2
         return np.exp(-gamma * distance)
     
